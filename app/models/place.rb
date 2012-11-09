5a3f2b742e0eaa6159d984153e59c599bca72cdc
agatov <agatovs@gmail.com>
2012-11-09 19:37:17 +0400

    added uploader for place and user avatars

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,5 @@
 class Place < ActiveRecord::Base
   attr_accessible :address, :avatar, :average_check, :description, :latitude, :longitude, :name, :phone
+
+  mount_uploader :avatar, PlaceAvatarUploader
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :avatar, :first_name, :last_name
+
+  mount_uploader :avatar, UserAvatarUploader
 end
