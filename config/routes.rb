48b213970695de528ac3cdd1ad41a40c1d77ac6b
agatov <agatovs@gmail.com>
2012-11-09 20:55:40 +0400

    authentications controller added

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Tastell::Application.routes.draw do
   resources :places, only: [:index, :show]
   resources :orders, only: [:index, :new, :create]
   resources :users, only: [:index]
+  resources :authentications, only: [:new, :create]
 
 end
