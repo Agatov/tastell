42f97b026c3d652a3811607be7e59e67ebe0b0a0
agatov <agatovs@gmail.com>
2012-11-15 20:27:31 +0400

    repost to vk

--- app/assets/javascripts/mobile/initializer.js.coffee
+++ app/assets/javascripts/mobile/initializer.js.coffee
@@ -3,11 +3,17 @@ $ ->
     ->
       $.post("/orders",
         {
-          content: $("#recommend-area").val()
+          "order[content]": $("#recommend-area").val(),
+          "order[place_id]": 1
         },
         (data) =>
           $("#recommend-form").hide()
+
+          $("#repost-form").find(".repost-button").attr("href", data.url)
+
           $("#repost-form").show()
           $("body").animate({scrollTop: 0})
+
+
       )
   )
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  # @param [Order] order
+  def order_social_params(order)
+    return {
+        url: "http://#{request.host}/places/#{order.place.id}##{order.id}",
+        title: CGI.escape("I get 30% sale to #{order.place.name} with Gurmap"),
+        description: CGI.escape("OLOLOLLO"),
+        image: "http://#{request.host}/images/logo.png"
+    }
+  end
+
+  # @param [Order] order
+  def vk_social_url(order)
+    params = order_social_params(order)
+    "http://vk.com/share.php?url=#{params[:url]}&title=#{params[:title]}&image=#{params[:image]}&description=#{params[:description]}&noparse=true"
+  end
 end
