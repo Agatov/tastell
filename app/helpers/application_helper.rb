18437852c412f779286f299cb05e67b57e85543d
agatov <agatovs@gmail.com>
2013-02-01 18:23:34 +0400

    mobile_browser detection added to application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,20 @@ module ApplicationHelper
     params = order_social_params(order)
     "http://vk.com/share.php?url=#{params[:url]}&title=#{params[:title]}&image=#{params[:image]}&description=#{params[:description]}&noparse=true"
   end
+
+  MOBILE_BROWSERS = [
+      'playbook', 'windows phone', 'android', 'ipod', 'iphone', 'opera mini', 'blackberry', 'palm','hiptop',
+      'avantgo','plucker', 'xiino','blazer','elaine', 'windows ce; ppc;', 'windows ce; smartphone;',
+      'windows ce; iemobile', 'up.browser','up.link','mmp','symbian','smartphone', 'midp','wap','vodafone',
+      'o2','pocket','kindle', 'mobile','pda','psp','treo'
+  ]
+
+  def mobile_browser?(agent)
+    agent.downcase!
+
+    MOBILE_BROWSERS.each do |m|
+      return true if agent.match(m)
+    end
+    false
+  end
 end
