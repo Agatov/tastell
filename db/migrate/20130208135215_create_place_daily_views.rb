cd6a7d6660592b8ec81b28550f059eb2bf32e494
agatov <agatovs@gmail.com>
2013-02-11 20:41:21 +0400

    migration changed

--- db/migrate/20130208135215_create_place_daily_views.rb
+++ db/migrate/20130208135215_create_place_daily_views.rb
@@ -1,14 +1,19 @@
 class CreatePlaceDailyViews < ActiveRecord::Migration
-  def change
+  def up
     create_table :place_daily_views do |t|
       t.integer :place_id
-      t.integer :total_hits_count, default: 0
-      t.integer :total_hosts_count, default: 0
+      t.integer :hosts_count, default: 0
+      t.integer :hits_count, default: 0
       t.integer :order_hits_count, default: 0
       t.integer :order_hosts_count, default: 0
+      t.integer :orders_count, default: 0
       t.date :date
 
       t.timestamps
     end
+  end
+
+  def down
+    drop_table :place_daily_views
   end
 end
