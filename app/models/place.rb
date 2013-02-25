31d37876f8916b797d86c078ff902217a0972e06
agatov <agatovs@gmail.com>
2013-02-25 17:42:23 +0400

    place_requisites - place relationship added

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,7 @@ class Place < ActiveRecord::Base
   attr_accessible :address, :avatar, :average_check, :description, :latitude, :longitude, :name, :phone, :logo
   attr_accessible :schedule, :public_phone, :kitchen, :metro, :active
 
+  has_one :place_requisites
   has_many :orders
   has_many :photos
   belongs_to :account
--- app/models/place_requisites.rb
+++ app/models/place_requisites.rb
@@ -1,3 +1,5 @@
 class PlaceRequisites < ActiveRecord::Base
   attr_accessible :bank, :bik, :correspondent_account, :inn_kpp, :legal_address, :ogrn, :place_id, :settlement_account
+
+  belongs_to :place
 end
